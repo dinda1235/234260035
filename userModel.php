429ce4af91a0c8766ff09f3598fca1fe9f08014d
dinda1235 <234260035.mhs@stmikjabar.ac.id>
2025-05-21 06:20:42 +0700

    Update userModel.php
    memperbarui komentar

--- userModel.php
+++ userModel.php
@@ -1,12 +1,13 @@
 <?php
 // app/Models/UserModel.php
 namespace App\Models; // Mendefinisikan namespace untuk model ini
-use CodeIgniter\Model; // Menggunakan class Model dari CodeIgniter
+use CodeIgniter\Model; // Mengimpor class Model dari framework CodeIgniter
 
 class UserModel extends Model { // Mendefinisikan class UserModel yang merupakan turunan dari Model
-    protected $table = 'users'; // Menentukan nama tabel yang digunakan
-    protected $allowedFields = ['username', 'password']; // Menentukan field yang diizinkan untuk diisi
-    protected $useTimestamps = false; // Menentukan apakah menggunakan timestamp atau tidak
+    protected $table = 'users'; // Menentukan nama tabel yang digunakan untuk menyimpan data user
+    protected $allowedFields = ['username', 'password']; // Menentukan field yang diizinkan untuk diisi dalam tabel
+    protected $useTimestamps = false; // Menentukan apakah menggunakan timestamp untuk mencatat waktu pembuatan atau pembaruan
 }
+
 
 ?>
